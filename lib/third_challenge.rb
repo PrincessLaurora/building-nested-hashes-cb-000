1dc762857b7f54807f17ef4dffbe7ccd460606c6
Laura Felce <crepu83@yahoo.it>
2018-11-26 11:21:38 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,10 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-      "Lord Capulet", age: "50"
+      name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        "Lady Capulet", age: "51"
+      name: "Lady Capulet", age: "51"
         },
       :heroine => {
         name: "Juliet", age: "15", status: "alive"
